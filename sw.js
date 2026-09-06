cc61bca449f18fce62d1185d684296b0caab4eb7
samehabu <aimer2430@gmail.com>
2026-09-06 16:53:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ADHD Field Guide - offline service worker (network-first) */
-const CACHE = 'adhd-guide-v12';
+const CACHE = 'adhd-guide-v14';
 const ASSETS = ['./', './index.html', './ar.html', './he.html', './manifest.json', './manifest-ar.json', './manifest-he.json', './icon-192.png', './icon-512.png', './config.js', './auth.js', './personalize.js', './signup-nudge.js', './bookmarks.js', './brain.js', './reports.js', './save-limit.js', './reveal-fix.js', './ads.js', './analytics.js'];
 
 self.addEventListener('install', (e) => {
